fdcb1e026cebb9d418dd96036510df9ee82be54a
victordmontero <victordanielmontero@gmail.com>
2017-02-23 16:37:00 -0400

    Commit inicial

---
+++ Helpers.Test/PersonaPruebas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpers;
+
+namespace Helpers.Test
+{
+    [TestClass]
+    public class PersonaPruebas
+    {
+        string[] cedulas;
+        [TestInitialize]
+        public void Setup()
+        {
+            cedulas = new string[] {
+                "40221867555", //true
+                "40221867556", //false
+                "40221868555", //false
+                "4022085225-1", //true
+                "402-2085225-1", //true
+                "412-2085225-1", //false
+                "402-2186855-8", //false
+                "402-218685A-8", //false
+                "402-218685A-08", //false
+            };
+        }
+
+        [TestMethod]
+        public void PruebaValidarCedula()
+        {
+            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[0]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[1]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[2]));
+            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[3]));
+            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[4]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[5]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[6]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[7]));
+            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[8]));
+        }
+    }
+}
---
+++ Helpers/PersonaHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class PersonaHelper
+    {
+        public static bool ValidarCedula(string cedula)
+        {
+            if (cedula.Any(c => char.IsLetter(c)))
+                return false;
+
+            cedula = cedula.Replace("-", "").Trim();
+            string temp = string.Empty;
+            if (cedula.Length == 11)
+            {
+                for (int i = 0; i < cedula.Length-1; i++)
+                {
+                    temp += (byte)char.GetNumericValue(cedula[i]) * (i % 2 + 1);
+                }
+                int sigma = temp.Select(x => (byte)char.GetNumericValue(x)).Sum(x => x);
+                return Math.Abs(10 - sigma % 10) == char.GetNumericValue(cedula.Last());
+            }
+            return false;
+        }
+    }
+}
