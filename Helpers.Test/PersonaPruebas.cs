0d1482d38d7eda31504465e7e893182fb4e62861
victordmontero <victordanielmontero@gmail.com>
2018-11-15 16:30:42 -0400

    Simplificando pruebas

--- Helpers.Test/PersonaPruebas.cs
+++ Helpers.Test/PersonaPruebas.cs
@@ -1,43 +1,37 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Helpers;
+using System.Collections.Generic;
 
 namespace Helpers.Test
 {
     [TestClass]
     public class PersonaPruebas
     {
-        string[] cedulas;
+        Dictionary<string, bool> cedulas;
         [TestInitialize]
         public void Setup()
         {
-            cedulas = new string[] {
-                "40221867555", //true
-                "40221867556", //false
-                "40221868555", //false
-                "4022085225-1", //true
-                "402-2085225-1", //true
-                "412-2085225-1", //false
-                "402-2186855-8", //false
-                "402-218685A-8", //false
-                "402-218685A-08", //false
-                "000-0000000-00" //false
-            };
+            cedulas = new Dictionary<string, bool>();
+            cedulas.Add("40221867555", true);
+            cedulas.Add("40221867556", false);
+            cedulas.Add("40221868555", false);
+            cedulas.Add("4022085225-1", true);
+            cedulas.Add("402-2085225-1", true);
+            cedulas.Add("412-2085225-1", false);
+            cedulas.Add("402-2186855-8", false);
+            cedulas.Add("402-218685A-8", false);
+            cedulas.Add("402-218685A-08", false);
+            cedulas.Add("000-0000000-00", false);
         }
 
         [TestMethod]
         public void PruebaValidarCedula()
         {
-            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[0]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[1]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[2]));
-            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[3]));
-            Assert.AreEqual(true, PersonaHelper.ValidarCedula(cedulas[4]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[5]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[6]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[7]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[8]));
-            Assert.AreEqual(false, PersonaHelper.ValidarCedula(cedulas[9]));
+            foreach (var cedula in cedulas)
+            {
+                Assert.AreEqual(cedula.Value, PersonaHelper.ValidarCedula(cedula.Key));
+            }
         }
     }
 }
